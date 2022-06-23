cfd03ed8b5535ceded62bffaf5b3bed81861a722
Simone Benedetti <simone.benedetti.416346@alumni.uniroma2.eu>
2022-06-23 18:17:44 +0200

    fix name array

--- lib/analytic.c
+++ lib/analytic.c
@@ -85,46 +85,46 @@ double rtime_mmk(double lambda, double mu, double k){
 void add_stats(){
 
 //blocco control unit
-statistics[0][0] = rtime_mm1(ARRIVAL_RATE,1.0/CONTROL_UNIT_SERVICE_TIME);
-statistics[0][1] = util_mm1(ARRIVAL_RATE,1.0/CONTROL_UNIT_SERVICE_TIME);
+analytics_statistics[0][0] = rtime_mm1(ARRIVAL_RATE,1.0/CONTROL_UNIT_SERVICE_TIME);
+analytics_statistics[0][1] = util_mm1(ARRIVAL_RATE,1.0/CONTROL_UNIT_SERVICE_TIME);
 
 //blocco video
-statistics[1][0] = rtime_mmkk(1.0/VIDEO_SERVICE_TIME);
-statistics[1][1] = util_mmkk(TH_VIDEO,1.0/CONTROL_UNIT_SERVICE_TIME,2);
+analytics_statistics[1][0] = rtime_mmkk(1.0/VIDEO_SERVICE_TIME);
+analytics_statistics[1][1] = util_mmkk(TH_VIDEO,1.0/CONTROL_UNIT_SERVICE_TIME,2);
 
 //blocco wifi
-statistics[2][0] = rtime_mmk(TH_WLAN,1.0/WLAN_FRAME_UPLOAD_TIME,2);
-statistics[2][1] = util_mmk(TH_WLAN,1.0/WLAN_FRAME_UPLOAD_TIME,2);
+analytics_statistics[2][0] = rtime_mmk(TH_WLAN,1.0/WLAN_FRAME_UPLOAD_TIME,2);
+analytics_statistics[2][1] = util_mmk(TH_WLAN,1.0/WLAN_FRAME_UPLOAD_TIME,2);
 
 //blocco rete cellulare
-statistics[3][0] = rtime_mm1(TH_ENODE,1.0/ENODE_FRAME_UPLOAD_TIME);
-statistics[3][1] = util_mm1(TH_ENODE,1.0/ENODE_FRAME_UPLOAD_TIME);
+analytics_statistics[3][0] = rtime_mm1(TH_ENODE,1.0/ENODE_FRAME_UPLOAD_TIME);
+analytics_statistics[3][1] = util_mm1(TH_ENODE,1.0/ENODE_FRAME_UPLOAD_TIME);
 
 //blocco edge
-statistics[4][0] = rtime_mmk(TH_EDGE,1.0/EDGE_PROCESSING_TIME,4);
-statistics[4][1] = util_mmk(TH_EDGE,1.0/EDGE_PROCESSING_TIME,4);
+analytics_statistics[4][0] = rtime_mmk(TH_EDGE,1.0/EDGE_PROCESSING_TIME,4);
+analytics_statistics[4][1] = util_mmk(TH_EDGE,1.0/EDGE_PROCESSING_TIME,4);
 
 //blocco cloud (utilizzazione tende a 0 al crescere di k serventi)
-statistics[5][0] = rtime_mmkk(1.0/WLAN_FRAME_UPLOAD_TIME);
-statistics[5][1] = 0;
+analytics_statistics[5][0] = rtime_mmkk(1.0/WLAN_FRAME_UPLOAD_TIME);
+analytics_statistics[5][1] = 0;
 
 }
 
 void print_stats(){
 
-    printf("Tempo risp control unit: %f\n", statistics[0][0]);
-    printf("Tempo risp video unit: %f\n", statistics[1][0]);
-    printf("Tempo risp wlan unit: %f\n", statistics[2][0]);
-    printf("Tempo risp enode unit: %f\n", statistics[3][0]);
-    printf("Tempo risp edge unit: %f\n", statistics[4][0]);
-    printf("Tempo risp cloud unit: %f\n", statistics[5][0]);
+    printf("Tempo risp control unit: %f\n", analytics_statistics[0][0]);
+    printf("Tempo risp video unit: %f\n", analytics_statistics[1][0]);
+    printf("Tempo risp wlan unit: %f\n", analytics_statistics[2][0]);
+    printf("Tempo risp enode unit: %f\n", analytics_statistics[3][0]);
+    printf("Tempo risp edge unit: %f\n", analytics_statistics[4][0]);
+    printf("Tempo risp cloud unit: %f\n", analytics_statistics[5][0]);
 
-    printf("Utilizzazione control unit: %f\n", statistics[0][1]);
-    printf("Utilizzazione video unit: %f\n", statistics[1][1]);
-    printf("Utilizzazione wlan unit: %f\n", statistics[2][1]);
-    printf("Utilizzazione enode unit: %f\n", statistics[3][1]);
-    printf("Utilizzazione edge unit: %f\n", statistics[4][1]);
-    printf("Utilizzazione cloud unit: %f\n", statistics[5][1]);
+    printf("Utilizzazione control unit: %f\n", analytics_statistics[0][1]);
+    printf("Utilizzazione video unit: %f\n", analytics_statistics[1][1]);
+    printf("Utilizzazione wlan unit: %f\n", analytics_statistics[2][1]);
+    printf("Utilizzazione enode unit: %f\n", analytics_statistics[3][1]);
+    printf("Utilizzazione edge unit: %f\n", analytics_statistics[4][1]);
+    printf("Utilizzazione cloud unit: %f\n", analytics_statistics[5][1]);
 
 }
 
